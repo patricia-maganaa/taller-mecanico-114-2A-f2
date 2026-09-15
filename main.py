3743a3b24eae014ae53140211ea2a95a5dadb3e5
Patricia Magaña <p.magana.ma@gmail.com>
2026-09-14 21:48:27 -0300

    test: probar multiples instancias de Vehiculo e independencia de estados en main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from vehiculo import Vehiculo
 
 v1 = Vehiculo("KXPR84", 2019)
+v2 = Vehiculo("JKLM12", 2016)
+
 v1.ingresar()
 
-print(v1.patente)
-print(v1.anio)
-print(v1.en_taller)
+print(v1.patente, v1.en_taller)
+print(v2.patente, v2.en_taller)
