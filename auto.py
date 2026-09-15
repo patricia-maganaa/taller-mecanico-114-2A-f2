5d42a7b431d9556431bc05918dbc7339d4279894
Patricia Magaña <p.magana.ma@gmail.com>
2026-09-14 21:58:39 -0300

    feat: crear clase Auto que hereda de Vehiculo en auto.py

---
+++ auto.py
@@ -0,0 +1,7 @@
+# Importa la clase base Vehiculo desde el módulo vehiculo
+from vehiculo import Vehiculo
+
+# Define la clase Auto que hereda de la clase base Vehiculo
+class Auto(Vehiculo):
+    # Declaración pass indica que por ahora la clase no añade métodos o atributos adicionales
+    pass
